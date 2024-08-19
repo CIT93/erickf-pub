fa3643f52d41f68b1d97191414b42a037662d136
EricksFlores <0836931@my.scccd.edu>
2024-08-19 17:12:32 +0000

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+// 1. count the members of your household
+const myMembers = 4;
+// 2. Consider the size of your home
+const mySize = 7;
+// 3.Evaluate your food choices
+const myFood = 8;
+// 4.Examine your water consumption
+const myWater = 0;
+// 5.Determine how many household purchases you make each year
+const myPurchases = 4;
+// 6.Consider how much waste you produce
+const myWaste = 20;
+// 7.Identify the amount of waste that you recycle.
+const myRecycle = 8;
+// 8.Tally up your annual transportation scores.
+const myTransport = 12;
+// 9.Add up your points. 
+const cfpTotal = myMembers + mySize + myFood + myWater + myPurchases + myWaste + myRecycle + myTransport
