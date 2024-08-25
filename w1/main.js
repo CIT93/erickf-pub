062c6a18ff1d2329e37df32ff2aa19f13f24cbe2
EricksFlores <0836931@my.scccd.edu>
2024-08-25 21:24:30 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,25 @@
-console.log('Hello World')
+console.log("hello from the inside the main.js file");
+
+//let myVar;
+
+const myVar = "erick";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow () {
+       if(myVarType === "number") {
+        console.log(`will 11 this one run?`);
+    } else {
+        console.log(`will 13 this one run?`);
+    }
+}
+
+runNow();
+runNow();
+
+
+
+
