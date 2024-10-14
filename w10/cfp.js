4e14c3bfcdbaedd6972f2a8f85e848b18c26ed05
EricksFlores <0836931@my.scccd.edu>
2024-10-14 16:00:19 +0000

    setup done

---
+++ w10/cfp.js
@@ -0,0 +1,33 @@
+const determineHouseHoldPts = function(numberInhousehold) {
+    let houseHoldPoints = 0;
+    if (numberInhousehold === 0) {
+      houseHoldPoints = 14;
+    } else if (numberInhousehold === 1) {
+        houseHoldPoints = 12;
+    } else if (numberInhousehold === 2) {
+        houseHoldPoints = 10;
+    } else if (numberInhousehold === 3) {
+        houseHoldPoints = 8;
+    } else if (numberInhousehold === 4) {
+        houseHoldPoints = 6;
+    } else if (numberInhousehold === 5) {
+        houseHoldPoints = 4;
+    } else if (numberInhousehold > 5) {
+        houseHoldPoints = 2;
+    }
+    return houseHoldPoints
+  }
+  const determineHs = function(houseSize) {
+    let houseHoldSizePTS = 0;
+    if (houseSize === "Large") {
+        houseHoldSizePTS = 10;
+    } else if (houseSize === "Medium") {
+        houseHoldSizePTS = 7;
+    } else if (houseSize === "Small") {
+        houseHoldSizePTS = 4;
+    }else if (houseSize === "Apartment") {
+        houseHoldSizePTS = 2;
+    }
+    return houseHoldSizePTS
+  }
+  export {determineHouseHoldPts, determineHs}
